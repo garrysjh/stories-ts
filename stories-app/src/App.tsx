86669010d7353e177b320a0c15e964d2c9f88606
garrysjh <garrysjh@gmail.com>
2023-05-25 10:30:22 +0800

    day3

--- stories-app/src/App.tsx
+++ stories-app/src/App.tsx
@@ -1,24 +1,27 @@
 import './App.css'
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 
-const Search = () => {
-  const [searchTerm, setSearchterm] = useState('');
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const search =(event.target as HTMLInputElement)
-    setSearchterm(search.value)
-  };
-  return ( 
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
-      <p>Searching for {searchTerm}</p>
-    </div>
-  )
-}
+interface InputWithLabelProps {
+  id: string;
+  label: string;
+  value: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+ }
+const InputWithLabel = ({id, label, value, onInputChange, type}: InputWithLabelProps) => (
+    <>
+      <label htmlFor={id}>{label} </label>
+      <input id={id} value={value} type={type} onChange={onInputChange}/>
+    </>
+    );
+
+  
 
 
 const App = () => {
+  const [searchTerm, setSearchterm] = useState(localStorage.getItem('search') || 'React');
   const stories = [
     {
       title: 'React',
@@ -37,19 +40,34 @@ const App = () => {
       objectID: 1,
     },
   ];
+  
+  const handleSearch = (event: React.FormEvent<HTMLInputElement>) => {
+    const search =(event.target as HTMLInputElement)
+    setSearchterm(search.value)
+  };
+
+  useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
+
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+
   return (
     <>
      <div>
       <h1>
         Hello World
       </h1>
-      <Search/>
-      <List list={stories}/>
+      <InputWithLabel id='search' label="Search: " type="text" value={searchTerm} onInputChange={handleSearch}/>
+      <List list={searchedStories}/>
     </div>
     </>
   )
 }
-function List(props: any){
+const List = (props: any) => {
   return (
     <ul>
       {props.list.map((item: any) => (
